4d811c424b7eb7e5ec6110b13fbf0826b2a9887d
Sai <saikrishnadas666@gmail.com>
2021-05-11 22:45:44 +0530

    adding contextAPI and other functionality

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Balance from "./components/Balance";
 import IncomeExpense from "./components/IncomeExpense";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +16,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </>
+    </GlobalProvider>
   );
 }
 
--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { uid } from "uid";
+
+import { GlobalContext } from "../context/GlobalState";
 
 function AddTransaction() {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: uid(10),
+      text,
+      amount: Number(amount),
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
